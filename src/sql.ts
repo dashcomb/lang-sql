7424c96e27fc5883c8e32abf950dfde2f52c8d57
Kent <719951601@qq.com>
2022-12-08 13:08:50 +0800

    test sql

--- src/sql.ts
+++ src/sql.ts
@@ -7,13 +7,9 @@ import {tokens, Dialect, tokensFor, SQLKeywords, SQLTypes, dialect} from "./toke
 import {completeFromSchema, completeKeywords} from "./complete"
 import {parseMixed} from "@lezer/common";
 import {parser as javascriptParser} from "@lezer/javascript"
-import {parser as jsParser} from "@codemirror/lang-js"
 
 let parser = baseParser.configure({
   wrap:parseMixed(node=>{
-    if(node.name==='QuotedIdentifier' || node.name==='String'){
-      return {parser: jsParser}
-    }
     if(node.name==='JsText'){
       return {parser: javascriptParser}
     }
